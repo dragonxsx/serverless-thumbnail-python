01983cec60b4119be7eee8f5b27da98a3fd36f1c
Long Nguyen <long_nguyen@epam.com>
2022-03-29 21:44:50 +0700

    Create REST API for thumbnail metadata

--- handler.py
+++ handler.py
@@ -91,3 +91,67 @@ def s3_save_thumbnail_url_to_dynamo(url_path, img_size):
         'headers': {'Content-Type': 'application/json'},
         'body': json.dumps(response)
     }
+
+def s3_get_thumbnail_urls(event, context):
+    table = dynamodb.Table(dbtable)
+    response = table.scan()
+    data = response['Items']
+
+    # paginate through the result in a loop
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+    
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(data)
+    }
+
+def s3_get_item(event, context):
+    table = dynamodb.Table(dbtable)
+    response = table.get_item(Key={
+        'id': event['pathParameters']['id']
+    })
+
+    item = response['Item']
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps(item),
+        'isBase64Encoded': False
+    }
+
+def s3_delete_item(event, context):
+    item_id = event['pathParameters']['id']
+
+    # Set the default error response
+    response = {
+        "statusCode": 500,
+        "body": f"An error occured while deleting post {item_id}"
+    }
+
+    table = dynamodb.Table(dbtable)
+    response = table.delete_item(Key={
+        'id': item_id
+    })
+    all_good_response = {
+        "deleted": True,
+        "itemDeletedId": item_id
+    }
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        response = {
+            "statusCode": 200,
+            "headers": {
+                'Content-Type' : 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            "body": json.dumps(all_good_response)
+        }
+
+    return response
